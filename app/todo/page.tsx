c00a3541e65dc6d4c5c3a5b301d264a647e25775
BLooDBRothER <computertechnology446@gmail.com>
2023-08-18 16:55:30 +0530

    initialize project

---
+++ app/todo/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Todo() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Todo</h1>
+    </main>
+  )
+}
---
+++ theme/themeConfig.ts
@@ -0,0 +1,10 @@
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+  "token": {
+    "colorPrimary": "#901ed6",
+    "colorInfo": "#901ed6"
+  }
+};
+
+export default theme;
