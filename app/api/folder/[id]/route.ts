2e007a916ff9df52f84f69f11b98d1d06d310749
BLooDBRothER <computertechnology446@gmail.com>
2023-09-05 00:54:38 +0530

    Update folder enpoint with only folder response

--- app/api/folder/[id]/route.ts
+++ app/api/folder/[id]/route.ts
@@ -1,10 +1,10 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/route";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma"
 import conn from "@/lib/db-pg";
 
-export async function GET(_: Request, { params }: { params: {id: string}}){
+export async function GET(request: NextRequest, { params }: { params: {id: string}}){
     const folderId = params.id;
 
     const session = await getServerSession(authOptions);
@@ -13,11 +13,15 @@ export async function GET(_: Request, { params }: { params: {id: string}}){
 
     const userId = session.user.id as string;
 
-    const query = "SELECT name, id FROM public.folders WHERE created_by = $1 AND parent_folder_id = $2 ORDER BY created_at";
-    const todoQuery = "SELECT title, id FROM public.todo WHERE created_by = $1 and folder_id = $2 ORDER BY created_at"
+    const onlyFolder  = request.nextUrl.searchParams.get("onlyFolder") ?? false; 
 
     try {
+        const query = "SELECT name, id FROM public.folders WHERE created_by = $1 AND parent_folder_id = $2 ORDER BY created_at";
         const data = await conn?.query(query, [userId, folderId]);
+        if(onlyFolder)
+            return NextResponse.json({"msg": "ok", folders: data.rows, todo: []})
+
+        const todoQuery = "SELECT title, id FROM public.todo WHERE created_by = $1 and folder_id = $2 ORDER BY created_at"
         const todoData = await conn?.query(todoQuery, [userId, folderId]);
         
         return NextResponse.json({"msg": "ok", folders: data.rows, todo: todoData.rows})
--- app/api/folder/route.ts
+++ app/api/folder/route.ts
@@ -1,20 +1,25 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma"
 import conn from "@/lib/db-pg";
 
-export async function GET(){
+export async function GET(request: NextRequest){
     const session = await getServerSession(authOptions);
 
     if(!session) return NextResponse.json({"message": "Please Login"}, {status: 401});
 
+    const onlyFolder  = request.nextUrl.searchParams.get("onlyFolder") ?? false; 
+
     const userId = session.user.id as string;
 
     const query = "SELECT name,id FROM public.folders WHERE created_by = $1 AND parent_folder_id IS NULL ORDER BY created_at"
-    const todoQuery = "SELECT title, id FROM public.todo WHERE created_by = $1 and folder_id IS NULL ORDER BY created_at"
-    
     const data = await conn?.query(query, [userId]);
+    
+    if(onlyFolder)
+        return NextResponse.json({"msg": "ok", folders: data.rows, todo: []})
+        
+    const todoQuery = "SELECT title, id FROM public.todo WHERE created_by = $1 and folder_id IS NULL ORDER BY created_at"
     const todoData = await conn?.query(todoQuery, [userId])
 
     return NextResponse.json({"msg": "ok", folders: data.rows, todo: todoData.rows})
