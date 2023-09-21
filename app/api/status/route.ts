23cb308610a5376bc15d8f575487372c5764b294
BLooDBRothER <computertechnology446@gmail.com>
2023-09-21 16:12:12 +0530

    Add endpoint for db health check

---
+++ app/api/status/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma"
+import { verifySignature } from "@/lib/verifySignature";
+
+export async function POST(request: Request) {
+    const signature = request.headers.get("upstash-signature");
+
+    if (!signature) {
+        return NextResponse.json({msg: "`Upstash-Signature` header is missing"}, { status: 400 })
+    }
+    
+    const keys = { currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY as string, nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY as string };
+
+    if(! await verifySignature({ signature }, keys)){
+        return NextResponse.json({msg: "Invalid Signature"}, { status: 403 })
+    }
+
+    await prisma.user.findFirst({});
+
+    return NextResponse.json({"status": true})
+}
