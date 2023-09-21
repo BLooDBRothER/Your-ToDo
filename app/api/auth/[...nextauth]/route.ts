40a84ec48da3f95a70f2d34144cbcc62d09ef20c
BLooDBRothER <computertechnology446@gmail.com>
2023-09-21 10:40:18 +0530

    Refactor auth from prisma to node-pg

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 import prisma from "@/lib/prisma"
+import conn from "@/lib/db-pg"
 
 export const authOptions = {
     // Configure one or more authentication providers
@@ -13,21 +14,29 @@ export const authOptions = {
     callbacks: {
         async signIn({ user }: any) {
 
-            const dbUser = await prisma.user.findUnique({
-                where: {
-                    email: user.email as string
-                }
-            })
+            const query = "SELECT * FROM public.users WHERE email = $1";
+            const res = await conn.query(query, [user.email]);
 
-            if(!dbUser){
-                await prisma.user.create({
-                    data: {
-                        email: user.email,
-                        profilePic: user.image,
-                        name: user.name
-                    }
-                })
+            if(res.rowCount === 0){
+                const userInsertQuery = "INSERT INTO public.users(email, profile_pic, name, updated_at) VALUES($1, $2, $3, now())"
+                const res = await conn.query(userInsertQuery, [user.email, user.image, user.name]);
+                console.log(res);
             }
+            // const dbUser = await prisma.user.findUnique({
+            //     where: {
+            //         email: user.email as string
+            //     }
+            // })
+
+            // if(!dbUser){
+            //     await prisma.user.create({
+            //         data: {
+            //             email: user.email,
+            //             profilePic: user.image,
+            //             name: user.name
+            //         }
+            //     })
+            // }
 
             return true
         },
@@ -36,12 +45,14 @@ export const authOptions = {
             return session
         },
         async jwt({ token }:any) {
-            const dbUser = await prisma.user.findUnique({
-                where: {
-                    email: token.email as string
-                }
-            })
-            token.id = dbUser?.id
+            const query = "SELECT * FROM public.users WHERE email = $1";
+            const res = await conn.query(query, [token.email]);
+            // const dbUser = await prisma.user.findUnique({
+            //     where: {
+            //         email: token.email as string
+            //     }
+            // })
+            token.id = res.rows[0]?.id
           return token
         }
       }
