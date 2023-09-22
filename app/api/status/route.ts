fa29a3c7d43d70c26660b03711a7b78cea9019a2
BLooDBRothER <computertechnology446@gmail.com>
2023-09-22 15:46:10 +0530

    Revert "Fallback for db coldstart"
    This reverts commit 3ea886e62677dc3dc51befce85e5e39c0e2f103a.

--- app/api/status/route.ts
+++ app/api/status/route.ts
@@ -3,24 +3,19 @@ import prisma from "@/lib/prisma"
 import { verifySignature } from "@/lib/verifySignature";
 
 export async function POST(request: Request) {
-    // const signature = request.headers.get("upstash-signature");
+    const signature = request.headers.get("upstash-signature");
 
-    // if (!signature) {
-    //     return NextResponse.json({msg: "`Upstash-Signature` header is missing"}, { status: 400 })
-    // }
+    if (!signature) {
+        return NextResponse.json({msg: "`Upstash-Signature` header is missing"}, { status: 400 })
+    }
     
-    // const keys = { currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY as string, nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY as string };
+    const keys = { currentSigningKey: process.env.QSTASH_CURRENT_SIGNING_KEY as string, nextSigningKey: process.env.QSTASH_NEXT_SIGNING_KEY as string };
 
-    // if(! await verifySignature({ signature }, keys)){
-    //     return NextResponse.json({msg: "Invalid Signature"}, { status: 403 })
-    // }
-
-    try{
-        await prisma.user.findFirst({});
-        return NextResponse.json({"status": true})
-    }
-    catch{
-        return NextResponse.json({"status": false})
+    if(! await verifySignature({ signature }, keys)){
+        return NextResponse.json({msg: "Invalid Signature"}, { status: 403 })
     }
 
+    await prisma.user.findFirst({});
+
+    return NextResponse.json({"status": true})
 }
